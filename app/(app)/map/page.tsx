eede60a3c9625245e30754a30ebac2b3c68f3103
Yuta Yamamoto <y.yamamoto@andeco.co.jp>
2026-07-04 01:37:39 +0900

    fix: dynamic import名の衝突を nextDynamic にリネームして解消

--- app/(app)/map/page.tsx
+++ app/(app)/map/page.tsx
@@ -1,9 +1,9 @@
-import dynamic from 'next/dynamic';
+import nextDynamic from 'next/dynamic';
 
 // マップはブラウザ専用APIを使うためSSRを無効化し、静的プリレンダリングもスキップ
 export const dynamic = 'force-dynamic';
 
-const MapClient = dynamic(() => import('./MapClient'), { ssr: false });
+const MapClient = nextDynamic(() => import('./MapClient'), { ssr: false });
 
 export default function MapPage() {
   return <MapClient />;
